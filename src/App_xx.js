e55ce2ab518f68e132e1d4189ca44a67196d0ae3
209410678 <209410678@gms.tku.rdu.tw>
2021-12-09 21:19:33 +0800

    first commit

---
+++ src/App_xx.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import T23_78 from './tutorial/2-useEffect/T23_78-useEffect-fetch-data';
+
+const App_xx = () => {
+  return (
+    <div className='container'>
+      <h2>Advanced Tutorial</h2>
+      <T23_78 />
+    </div>
+  );
+};
+
+export default App_xx;
---
+++ src/tutorial/1-useState/T13_78-useState-array.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { data } from '../../data';
+
+const T13_78_UseStateArray = () => {
+  const [people, setPeople] = React.useState(data);
+  console.log('people', people)
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((p) => p.id !== id);
+    setPeople(newPeople);
+  }
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div id={id} className='item'>
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>Remove</button>
+          </div>
+        )
+      })}
+      <button className='btn' onClick={() => setPeople([])}>Clear Item</button>
+    </>
+  );
+};
+
+export default T13_78_UseStateArray;
