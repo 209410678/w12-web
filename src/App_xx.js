6f00588c4f87c2aedae54298cf7e77c3fddad3ab
209410678 <209410678@gms.tku.edu.tw>
2021-12-16 20:07:32 +0800

    Initial commit

--- src/App_xx.js
+++ src/App_xx.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import T23_78 from './tutorial/2-useEffect/T23_78-useEffect-fetch-data';
+import T33_78 from './tutorial/3-conditional-rendering/T33_78_show-hide';
 
 const App_xx = () => {
   return (
     <div className='container'>
       <h2>Advanced Tutorial</h2>
-      <T23_78 />
+      <T33_78 />
     </div>
   );
 };
