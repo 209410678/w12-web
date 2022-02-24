8a18c2ecbe648cfe82c3cd504248b3523d3c6739
209410678 <209410678@gms.tku.edu.tw>
2022-02-24 20:59:49 +0800

    first commit

---
+++ src/App_78.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import T6_78 from './tutorial/6-useReducer/T6_78_index2'
+
+
+const App_78 = () => {
+
+  return (
+ <div className='container'>
+     <h2>Tutorial 6 (v2)- 209410678</h2>
+       <T6_78 />
+     </div>
+  );
+};
+
+export default App_78;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App_xx from './App_xx';
+import App_78 from './App_78';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App_xx />
+    <App_78 />
   </BrowserRouter>,
   document.getElementById('root')
 );
